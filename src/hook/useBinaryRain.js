884519a8d34d7f213ad1c36a5afceaadf566f6de
EyalShapiro <eyalshapiro24@gmail.com>
2025-02-01 00:34:14 +0200

    useBinaryRain hook for improved customization

--- src/hook/useBinaryRain.js
+++ src/hook/useBinaryRain.js
@@ -1,7 +1,13 @@
 import { useEffect, useRef } from "react";
 
 export const useBinaryRain = () => {
+	const text = "i-tech",
+		colorBin = "#0F0",
+		speed = 0.75,
+		fontSize = 16,
+		backgroundColor = "rgba(10, 10, 10, 0.1)";
 	const canvasRef = useRef(null);
+	const animationRef = useRef(null);
 
 	useEffect(() => {
 		const canvas = canvasRef.current;
@@ -17,50 +23,50 @@ export const useBinaryRain = () => {
 		resizeCanvas();
 		window.addEventListener("resize", resizeCanvas);
 
-		const fontSize = 16;
 		const columns = Math.floor(canvas.width / fontSize);
 		const drops = Array(columns).fill(1);
 		const binaryChars = "01";
 
 		const drawBinaryRain = () => {
-			ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
+			ctx.fillStyle = backgroundColor;
 			ctx.fillRect(0, 0, canvas.width, canvas.height);
+			ctx.fillStyle = colorBin;
 
-			ctx.fillStyle = "#0F0";
 			ctx.font = `${fontSize}px monospace`;
 
 			for (let i = 0; i < drops.length; i++) {
-				const text = binaryChars[Math.floor(Math.random() * binaryChars.length)];
-				ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+				const mainText = binaryChars[Math.floor(Math.random() * binaryChars.length)];
+				ctx.fillText(mainText, i * fontSize, drops[i] * fontSize);
 
-				if (drops[i] * fontSize > canvas.height && Math.random() > 0.98) {
-					drops[i] = 0;
-				}
-				drops[i]++;
+				if (drops[i] * fontSize > canvas.height && Math.random() > 0.98) drops[i] = 0;
+
+				drops[i] += speed; // Adjust falling speed
 			}
 
 			drawText();
 		};
 
 		const drawText = () => {
-			const text = "i-tech";
-			ctx.font = "50px monospace";
-			ctx.fillStyle = "rgba(255, 255, 255, 0.9)";
+			ctx.font = "180px monospace";
+			ctx.fillStyle = "rgba(20, 200, 200, 0.9)";
+			ctx.shadowBlur = 20;
+
 			const textWidth = ctx.measureText(text).width;
 			ctx.fillText(text, (canvas.width - textWidth) / 2, canvas.height / 2);
 		};
 
 		const animate = () => {
 			drawBinaryRain();
-			requestAnimationFrame(animate);
+			animationRef.current = requestAnimationFrame(animate);
 		};
 
 		animate();
 
 		return () => {
 			window.removeEventListener("resize", resizeCanvas);
+			cancelAnimationFrame(animationRef.current);
 		};
-	}, []);
+	}, [text, colorBin, speed, fontSize, backgroundColor]);
 
 	return { canvasRef };
 };
